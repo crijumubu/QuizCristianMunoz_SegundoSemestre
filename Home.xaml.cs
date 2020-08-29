e8b1ad05fb7e098d51933f9d5fc5f2ab7353a95e
Cristian Julian Muñoz Buenahora <62572462+crijumubu@users.noreply.github.com>
2020-08-28 20:30:44 -0500

    Creación de la aplicación

---
+++ Home.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = txtProductName.Text;
+            string category = lblCategory.Content.ToString();
+            txtResults.Text = productName+"\n"+category;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.IsChecked.Value) 
+            {
+                lblCategory.Content = rb.Content.ToString();
+            }
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "Cristian" && txtPassword.Password == "1234")
+            {
+                MainWindow w = (MainWindow) Window.GetWindow(this);
+                w.frameMain.NavigationService.Navigate(new Home());
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña inconrrecta");
+            }
+        }
+    }
+}
